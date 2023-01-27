30e74354ef3db99afa509831f5f51b9c70c6c5fb
zarathacia <aya.benslimen@supcom.tn>
2023-01-27 22:39:03 +0100

    config

--- .circleci/ansible/roles/configure-server/files/myenv.sh
+++ .circleci/ansible/roles/configure-server/files/myenv.sh
@@ -1,9 +1,9 @@
 export TYPEORM_CONNECTION=postgres
 export TYPEORM_ENTITIES=./src/modules/domain/**/*.entity.ts
-export TYPEORM_HOST=database-2.ctupcjqkfsj5.us-east-1.rds.amazonaws.com
+export TYPEORM_HOST=udapeople-db.c7kuq301ibf7.us-east-1.rds.amazonaws.com
 export TYPEORM_PORT=5432
 export TYPEORM_USERNAME=postgres
-export TYPEORM_PASSWORD=postgres
+export TYPEORM_PASSWORD=udapeople123
 export TYPEORM_DATABASE=postgres
 export TYPEORM_MIGRATIONS=./src/migrations/*.ts
 export TYPEORM_MIGRATIONS_DIR=./src/migrations
